c8b27b6e3e9715c5c3ea8196ec7e8c17d4c92e2d
Noriclol <maxv.personligt@gmail.com>
2024-07-25 13:41:15 +0200

    removed scrapped scripts

--- scripts/scrapped/LocalPlayerData.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-class_name LocalPlayerData
-
-@export var tank_name : String = "Tank"
-@export var kills : int = 0
-@export var player_id : int
-@export var player_team : int
--- scripts/scrapped/debug_ui.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-class_name Debug_UI
-
-@onready var engine_status : Label = %Engine_Status_Value
-@onready var current_gear : Label = %Current_Gear_Value
-@onready var engine_force : Label = %Engine_Force_Value
